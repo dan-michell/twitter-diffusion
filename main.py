45df3fbca6364152002a3d908a30f90558e45295
dan-michell <dan.michell1000@gmail.com>
2022-10-17 22:28:02 +0100

    authentication working

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import tweepy
-import json
+import os
+
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    
-    auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-    auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+    load_dotenv("auth.env")
+
+    auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_KEY_SECRET"))
+    auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
     
     api = tweepy.API(auth)
 
     try:
         api.verify_credentials()
         print("Authentication OK")
-    except:
-        print("Error during authentication")
+    except Exception as e:
+        print(e)
